38b7c3b37fae62e2aa6efcdfbd22e9dc260075d0
Ilya Redko <trytosolve@gmail.com>
2018-05-07 21:52:33 +0300

    fixed

--- src/main/java/com/iredko/wowcraft/controllers/MainController.java
+++ src/main/java/com/iredko/wowcraft/controllers/MainController.java
@@ -2,12 +2,14 @@ package com.iredko.wowcraft.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/")
 public class MainController {
 
+    @RequestMapping(method= RequestMethod.GET)
     public ModelAndView showMainPage(ModelAndView modelAndView) {
         modelAndView.setViewName("mainPage");
         return modelAndView;
