ef5c12385e3021a247e81a7d749f119e1d4e31d7
trytosolve <trytosolve@gmail.com>
2018-05-18 16:41:33 +0300

    fixed add recipe with new delete logic

--- src/main/webapp/resources/js/reagentCreation.js
+++ src/main/webapp/resources/js/reagentCreation.js
@@ -22,18 +22,17 @@ $(document).ready(function () {
         var clone = $("[id^=clone_reagent]:last").clone();
         var lastId = clone.attr('id');
         var lastIdValue = lastId.replace('clone_reagent','');
-        alert(lastIdValue);
         clone_id = Number(lastIdValue)+1;
         clone.attr('id', 'clone_reagent' + clone_id);
-        clone.find("cboReagents").attr('id', 'cboReagents' + clone_id);
-        clone.find("#empty_option").attr("selected", "selected");
-        clone.find("#count").attr("value", "");
-        clone.find("#count").attr('id', 'count' + clone_id);
-        clone.find("#delete").removeAttr("hidden");
-        clone.find("#delete").attr("id","delete"+clone_id);
-        clone.find("#delete").attr("value",clone_id);
+        clone.find("#cboReagents"+lastIdValue).attr('id', 'cboReagents' + clone_id)
+        clone.find("#empty_option"+lastIdValue).attr('id', 'empty_option' + clone_id);
+        clone.find("#empty_option"+clone_id).attr("selected", "selected");
+        clone.find("#count"+lastIdValue).attr("value", "");
+        clone.find("#count"+lastIdValue).attr('id', 'count' + clone_id);
+        clone.find("#delete"+lastIdValue).removeAttr("hidden");
+        clone.find("#delete"+lastIdValue).attr("id","delete"+clone_id);
+        clone.find("#delete"+lastIdValue).attr("value",clone_id);
         clone.appendTo("#recipe_property");
-        clone_id = clone_id + 1;
     });
 });
 
